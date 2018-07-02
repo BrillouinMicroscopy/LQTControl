30fc0a5c37d80003596d7f0cddc5fbc7fd6515ab
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-07-02 17:49:40 +0200

    Use templates

--- LQTControl/generalmath.h
+++ LQTControl/generalmath.h
@@ -22,8 +22,9 @@ public:
 		return std::accumulate(std::begin(vector), std::end(vector), std::complex<double>(0.0, 0.0)) / std::complex<double>(vector.size(), 0);
 	}
 
-	static double max(std::vector<double> vector) {
-		double max = vector[0];
+	template <typename T = double>
+	static T max(std::vector<T> vector) {
+		T max = vector[0];
 		for (int jj(0); jj < vector.size(); jj++) {
 			if (vector[jj] > max) {
 				max = vector[jj];
@@ -32,18 +33,9 @@ public:
 		return max;
 	}
 
-	static int32_t max(std::vector<int32_t> vector) {
-		int32_t max = vector[0];
-		for (int jj(0); jj < vector.size(); jj++) {
-			if (vector[jj] > max) {
-				max = vector[jj];
-			}
-		}
-		return max;
-	}
-
-	static double min(std::vector<double> vector) {
-		double min = vector[0];
+	template <typename T = double>
+	static T min(std::vector<T> vector) {
+		T min = vector[0];
 		for (int jj(0); jj < vector.size(); jj++) {
 			if (vector[jj] < min) {
 				min = vector[jj];
@@ -52,26 +44,9 @@ public:
 		return min;
 	}
 
-	static int32_t min(std::vector<int32_t> vector) {
-		int32_t min = vector[0];
-		for (int jj(0); jj < vector.size(); jj++) {
-			if (vector[jj] < min) {
-				min = vector[jj];
-			}
-		}
-		return min;
-	}
-
-	static double absSum(std::vector<double> vector) {
-		double sum = 0;
-		for (int jj(0); jj < vector.size(); jj++) {
-			sum += abs(vector[jj]);
-		}
-		return sum;
-	}
-
-	static int32_t absSum(std::vector<int32_t> vector) {
-		int32_t sum = 0;
+	template <typename T = double>
+	static T absSum(std::vector<T> vector) {
+		T sum = 0;
 		for (int jj(0); jj < vector.size(); jj++) {
 			sum += abs(vector[jj]);
 		}
