ef9dd85dc7ab032174c16f35b591dcb831be8ff9
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2019-11-26 14:48:04 +0100

    Fix signed-unsigned mismatch warning

--- LQTControl/src/locking.cpp
+++ LQTControl/src/locking.cpp
@@ -211,7 +211,7 @@ void Locking::lock() {
 
 	// write data to array for plotting
 	m_lockDataPlot[static_cast<int>(lockViewPlotTypes::TRANSMISSION)].clear();
-	for (gsl::index j{ 0 }; j < lockData.transmission.size(); j++) {
+	for (gsl::index j{ 0 }; j < (gsl::index)lockData.transmission.size(); j++) {
 		m_lockDataPlot[static_cast<int>(lockViewPlotTypes::TRANSMISSION)].append(QPointF(lockData.relTime[j], lockData.transmission[j]));
 	}
 
